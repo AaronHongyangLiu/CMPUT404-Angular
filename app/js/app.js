52ada97785eee6a286458d2cbb6d00bd97cde6c3
Abram Hindle <abram.hindle@ualberta.ca>
2014-03-25 11:38:30 -0600

    app example

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
   'ngRoute',
@@ -9,9 +8,10 @@ angular.module('myApp', [
   'myApp.directives',
   'myApp.controllers'
 ]).
+// Deal with the hash routes like #/view2
+// Tells the app which controller to use for each route
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-/*  $routeProvider.when('/example', {templateUrl: 'partials/example.html', controller: 'MyExample'}); */
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/view1'}); // add a default
 }]);
