955219a7e91aa0da92e76da7ea9c1f147131f777
Abram Hindle <abram.hindle@ualberta.ca>
2014-03-26 16:52:18 -0600

    tuple example

--- app/js/app.js
+++ app/js/app.js
@@ -14,5 +14,12 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.when('/example', {templateUrl: 'partials/example.html', controller: 'MyExample'});
+  $routeProvider.when('/tuples', {templateUrl: 'partials/tuples.html', controller: 'Tuples'});
   $routeProvider.otherwise({redirectTo: '/view1'}); // add a default
-}]);
+}])
+.directive('myTuple', function() {
+   return {
+      restrict: 'E', //match element name
+      templateUrl: 'partials/myTuple.html'
+   };
+});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,6 +9,19 @@ angular.module('myApp.controllers', []).
   .controller('MyCtrl2', [function() {
 
   }])
-    .controller('MyExample', function($scope) {
+  .controller('MyExample', function($scope) {
       $scope.example = "Example";
-  });
+      $scope.tuple = {first: "first", rest: "rest"};
+  })
+  .controller('Tuples', ['$scope',function($scope) {
+      $scope.tuples = [{first: "first", rest:"some stuff"},
+                       {first: "second", rest:"more stuff"},
+                       {first: "third", rest:"more stuff"},
+                       {first: "fourth", rest:"more stuff"},
+                       {first: "fifth", rest:"more stuff"}];
+      $scope.addTuple = function() {
+          $scope.tuples.push({first:"new",rest:Math.random()});
+      };
+      
+
+  }]);
